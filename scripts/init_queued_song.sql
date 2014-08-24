3c6d7d670bc911f29ccf9470028279a40bbab7ea
math4origami <math4origami@gmail.com>
2014-08-23 19:13:52 -0700

    adding init queued song

---
+++ scripts/init_queued_song.sql
@@ -0,0 +1,11 @@
+
+CREATE TABLE IF NOT EXISTS `queued_song` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `queue_id` int(11) NOT NULL,
+  `queue_index` int(11) NOT NULL AUTO_INCREMENT,
+  `type` int(11) NOT NULL DEFAULT '0',
+  `name` varchar(255) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `queue_id` (`queue_id`,`queue_index`)
+);
