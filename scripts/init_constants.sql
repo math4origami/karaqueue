f1d7442c4d9bc216591d3af50ee2edf5805b4048
Russell chou <math4origami@gmail.com>
2016-11-03 19:22:58 -0700

    key is keyword in sql

--- scripts/init_constants.sql
+++ scripts/init_constants.sql
@@ -3,7 +3,7 @@ USE karaqueue;
 
 CREATE TABLE IF NOT EXISTS `constants` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `key` varchar(255) NOT NULL,
+  `constant_key` varchar(255) NOT NULL,
   `value` varchar(255) NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
